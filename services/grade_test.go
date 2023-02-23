7e47818ebe14ab640eb0cb72aaf8d91e654388eb
lebrancconvas <lebrancconvas@gmail.com>
2023-02-23 12:11:14 +0700

    ✅ test: run sub test

--- services/grade_test.go
+++ services/grade_test.go
@@ -5,24 +5,32 @@ import (
 	"testing"
 )
 
-func TestCheckGradeA(t *testing.T) {
-	grade := services.CheckGrade(80)
-	expected := "A"
+func TestCheckGrade(t *testing.T) {
+	t.Run("A", func(t *testing.T) {
+		grade := services.CheckGrade(80)
+		expected := "A"
 
-	if grade != expected {
-		t.Errorf("Expected %s but got %s", expected, grade)
-	}
+		if grade != expected {
+			t.Errorf("Expected %s but got %s", expected, grade)
+		}
+	})
+	t.Run("B", func(t *testing.T) {
+		grade := services.CheckGrade(70)
+		expected := "B"
+
+		if grade != expected {
+			t.Errorf("Expected %s but got %s", expected, grade)
+		}
+	})
+	t.Run("C", func(t *testing.T) {
+		grade := services.CheckGrade(60)
+		expected := "C"
+
+		if grade != expected {
+			t.Errorf("Expected %s but got %s", expected, grade)
+		}
+	})
 }
-
-func TestCheckGradeB(t *testing.T) {
-	grade := services.CheckGrade(70)
-	expected := "B"
-
-	if grade != expected {
-		t.Errorf("Expected %s but got %s", expected, grade)
-	}
-}
-
 func TestHello(t *testing.T) {
 
 }
